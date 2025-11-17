d75afb568c51aa2f4fda313cdd67fcfa344f89b2
Tait Hoyem <tait@tait.tech>
2025-11-16 22:09:18 -0700

    Add demo presentation, recording to db

--- syncslide-websocket/migrations/20251116212111_recording.up.sql
+++ syncslide-websocket/migrations/20251116212111_recording.up.sql
@@ -2,16 +2,18 @@
 CREATE TABLE IF NOT EXISTS recording (
 	id INTEGER NOT NULL PRIMARY KEY,
 	presentation_id INTEGER NOT NULL,
+	name TEXT NOT NULL,
 	vtt_path TEXT NOT NULL,
 	captions_path TEXT NOT NULL,
 	video_path TEXT NOT NULL,
-	start DATETIME NOT NULL DEFAULT current_timestamp,
+	start DATETIME NOT NULL DEFAULT (strftime('%s', 'now')),
 	FOREIGN KEY(presentation_id) REFERENCES presentation(id)
 );
-INSERT INTO recording (id, presentation_id, vtt_path, captions_path, video_path) VALUES (
+INSERT INTO recording (id, presentation_id, name, vtt_path, captions_path, video_path) VALUES (
 	1,
 	-- fixed for demo presetation
 	1,
+	'First Try of Demo',
 	'recording.vtt',
 	'captions.vtt',
 	'demo.mp4'
